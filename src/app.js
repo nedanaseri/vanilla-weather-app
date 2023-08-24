90c7b74ebac0f3b7c0343ed5b1eaed86ac2df2df
nedanaseri <nedanaseri24@gmail.com>
2023-08-25 00:00:45 +0330

    Updated time & date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,46 @@
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let dayNumber = date.getDate();
+  let months = [
+    "Januaray",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  return `${day}, ${dayNumber} ${month} ${year} | ${hours} : ${minutes} ${ampm}`;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city-name");
@@ -5,16 +48,19 @@ function displayTemperature(response) {
   let feelsLikeElement = document.querySelector("#feels-like");
   let humidityElement = document.querySelector("#humidity-level");
   let windElement = document.querySelector("#wind-speed");
+  let dateHourElement = document.querySelector("#date-hour");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
-  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateHourElement.innerHTML = formateDate(response.data.time * 1000);
 }
 
 let apiKey = "a31b0012587tao35f4af0fb35a58b69f";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Paris}&key=${apiKey}&units=metric`;
+let city = "Amsterdam";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
