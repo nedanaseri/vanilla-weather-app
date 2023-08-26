4e02c48349d64b7f7a5d4eba98adfdccf2cc9fbf
nedanaseri <nedanaseri24@gmail.com>
2023-08-26 13:28:12 +0330

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -50,6 +50,8 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind-speed");
   let iconELement = document.querySelector("#weather-icon");
 
+  celsiusTemperature = response.data.main.temp;
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -132,12 +134,6 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-let currentLocation = document.querySelector("#location-button");
-currentLocation.addEventListener("click", getLocation);
-
 function getPosition(position) {
   let apiKey = "1fd8093fa5ff12d796d7de756cc9d6b9";
   let lat = position.coords.latitude;
@@ -150,3 +146,36 @@ function getPosition(position) {
 function getLocation() {
   navigator.geolocation.getCurrentPosition(getPosition);
 }
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiuslink.classList.remove("active");
+  fahrenheitlink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiuslink.classList.add("active");
+  fahrenheitlink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocation = document.querySelector("#location-button");
+currentLocation.addEventListener("click", getLocation);
+
+let fahrenheitlink = document.querySelector("#fahrenheit-link");
+fahrenheitlink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiuslink = document.querySelector("#celsius-link");
+celsiuslink.addEventListener("click", displayCelsiusTemperature);
+
+search("Tehran");
